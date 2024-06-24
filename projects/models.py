89d6972654d837d0287e16c3d8794d35e1431a17
Konsing <ham.konsing@gmail.com>
2024-06-23 21:30:44 -0700

    Added files

---
+++ projects/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='project_images/')
+
+    def __str__(self):
+        return self.title
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
